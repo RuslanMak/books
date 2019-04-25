dd4dc5d77fa27aa3308a2711993f5708855628b7
RuslanMak <makarovemd@gmail.com>
2019-04-25 21:08:00 +0300

    10. update LIMIT - did GROUP middleware

--- routes/web.php
+++ routes/web.php
@@ -11,37 +11,39 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'api'], function () {
 
-Route::get('/books', 'BooksController@index');
-Route::get('/api-all-books/{skip}/{take}', 'BooksController@allBooks')->middleware('api');
-Route::get('/api-all-writers/{skip}/{take}', 'WritersController@allWritersPaginated')->middleware('api');
-Route::get('/api-all-writers', 'WritersController@allWriters')->middleware('api');
-Route::get('/api-all-genres', 'GenresController@allGenres')->middleware('api');
-Route::post('/api-save-book', 'BooksController@store')->middleware('api');
-Route::get('/api-book/{id}', 'BooksController@show')->middleware('api');
-Route::delete('/api-book-delete/{id}', 'BooksController@destroy')->middleware('api');
-Route::post('/api-book-update/{id}', 'BooksController@update')->middleware('api');
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/api-filtered-books/{skip}/{take}/', 'BooksController@filteredBooks')->middleware('api');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/books', 'BooksController@index');
+    Route::get('/api-all-books/{skip}/{take}', 'BooksController@allBooks');
+    Route::get('/api-all-writers/{skip}/{take}', 'WritersController@allWritersPaginated');
+    Route::get('/api-all-writers', 'WritersController@allWriters');
+    Route::get('/api-all-genres', 'GenresController@allGenres');
+    Route::post('/api-save-book', 'BooksController@store');
+    Route::get('/api-book/{id}', 'BooksController@show');
+    Route::delete('/api-book-delete/{id}', 'BooksController@destroy');
+    Route::post('/api-book-update/{id}', 'BooksController@update');
 
-Route::get('/writers', 'WritersController@index');
-Route::post('/api-save-writer', 'WritersController@store')->middleware('api');
-Route::get('/api-writer/{id}', 'WritersController@show')->middleware('api');
-Route::post('/api-writer-update/{id}', 'WritersController@update')->middleware('api');
-Route::delete('/api-writer-delete/{id}', 'WritersController@destroy')->middleware('api');
-Route::get('/api-filtered-writers/{skip}/{take}/', 'WritersController@filter')->middleware('api');
+    Route::get('/api-filtered-books/{skip}/{take}/', 'BooksController@filteredBooks');
 
-Route::get('/genres', 'GenresController@index');
-Route::get('/api-all-genres/{skip}/{take}', 'GenresController@allPaginated')->middleware('api');
-Route::get('/api-filtered-genres/{skip}/{take}/', 'GenresController@filter')->middleware('api');
-Route::post('/api-save-genres', 'GenresController@store')->middleware('api');
-Route::get('/api-genre/{id}', 'GenresController@show')->middleware('api');
-Route::post('/api-genre-update/{id}', 'GenresController@update')->middleware('api');
-Route::delete('/api-genre-delete/{id}', 'GenresController@destroy')->middleware('api');
+    Route::get('/writers', 'WritersController@index');
+    Route::post('/api-save-writer', 'WritersController@store');
+    Route::get('/api-writer/{id}', 'WritersController@show');
+    Route::post('/api-writer-update/{id}', 'WritersController@update');
+    Route::delete('/api-writer-delete/{id}', 'WritersController@destroy');
+    Route::get('/api-filtered-writers/{skip}/{take}/', 'WritersController@filter');
+
+    Route::get('/genres', 'GenresController@index');
+    Route::get('/api-all-genres/{skip}/{take}', 'GenresController@allPaginated');
+    Route::get('/api-filtered-genres/{skip}/{take}/', 'GenresController@filter');
+    Route::post('/api-save-genres', 'GenresController@store');
+    Route::get('/api-genre/{id}', 'GenresController@show');
+    Route::post('/api-genre-update/{id}', 'GenresController@update');
+    Route::delete('/api-genre-delete/{id}', 'GenresController@destroy');
+});
